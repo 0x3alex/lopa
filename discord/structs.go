5f0ac47c8d2bb8c6a38251574a13304fd44cec3e
Alex <a-wrede@outlook.de>
2022-11-28 14:19:38 +0100

    fixed json fields

--- discord/structs.go
+++ discord/structs.go
@@ -4,12 +4,12 @@ import "github.com/0x3alex/lopa/apis"
 
 type (
 	Bot struct {
-		Name          string `json:name`
-		Token         string `json:token`
-		Prefix        string `json:prefix`
-		Status        string `json:status`
-		SpotifyID     string `json:spotifyid`
-		SpotifySecret string `json:spotifysecret`
+		Name          string `json:"name"`
+		Token         string `json:"token"`
+		Prefix        string `json:"prefix"`
+		Status        string `json:"status"`
+		SpotifyID     string `json:"spotifyid"`
+		SpotifySecret string `json:"spotifysecret"`
 		Spotify       *apis.Spotify
 	}
 	Command struct {
